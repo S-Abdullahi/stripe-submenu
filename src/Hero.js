33721643ce5736c4099fe1cd8b06624c0db5c323
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-02-10 18:41:10 +0100

    styling

--- src/Hero.js
+++ src/Hero.js
@@ -1,7 +1,17 @@
 import React from "react";
+import phone from './images/phone.svg'
 
-export default function Hero(){
-    return <>
-        <h1>Hero</h1>
-    </>
+export default function Hero() {
+  return (
+    <section className="hero">
+      <article className="hero-text-con">
+        <h1 className="hero-heading">Payments infrastructure <br></br>for the internet</h1>
+        <p className="hero-info">Millions of companies of all sizes—from startups to Fortune 500s—use Stripe’s software and APIs to accept payments, send payouts, and manage their businesses online.</p>
+        <button className="btn startup-btn">Start now</button>
+      </article>
+      <div className="hero-image-con">
+        <img src={phone} alt="phone" className="hero-image"/>
+      </div>
+    </section>
+  );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,29 @@
 import React from "react";
-import {FaBars} from 'react-icons/fa'
-import logo from './images/logo.svg'
+import { FaBars } from "react-icons/fa";
+import logo from "./images/logo.svg";
+import { sublinks } from "./Data";
 
-export default function NavBar(){
-    return <nav>
-        <div>
-            <img src={logo} alt='stripe'/>
-            <FaBars/>
+export default function NavBar() {
+  return (
+    <nav className="nav-con">
+      <div className="nav-header">
+        <img src={logo} alt="stripe" className="logo" />
+        <div className="menu-bar-con">
+          <FaBars className="menu-bar" />
         </div>
+      </div>
+      <ul className="menu-con">
+        <li className="menu-item">
+          <button className="menu-btn">products</button>
+        </li>
+        <li className="menu-item">
+          <button className="menu-btn">developers</button>
+        </li>
+        <li className="menu-item">
+          <button className="menu-btn">company</button>
+        </li>
+      </ul>
+      <button className="btn signin-btn">Sign in</button>
     </nav>
+  );
 }
