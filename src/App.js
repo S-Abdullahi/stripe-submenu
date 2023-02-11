98cad89bd39fd25a25acb684a73a5fc16465f2d2
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-02-11 23:00:25 +0100

    sidebar modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "./Navbar";
+import SideBar from "./Sidebar";
 import Hero from "./Hero";
 
 
 export default function App(){
-    return <>
+    return <div className="main">
         <NavBar/>
+        <SideBar/>
         <Hero />
-    </>
+    </div>
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { FaCreditCard, FaBook, FaBriefcase, FaTimes } from "react-icons/fa";
+import sublinks from "./Data";
 
-export default function Sidebar(){
-    return <>
-        <h1>Sidebar</h1>
-    </>
+export default function Sidebar() {
+  return (
+    <aside className="sidebar-con">
+      <button className="sidebar-cancel-btn">
+        <FaTimes className="sidebar-cancel" />
+      </button>
+      <div className="sidebar-item">
+        {sublinks.map(({ page, links }) => {
+          return (
+            <>
+              <h4 className="sidebar-heading">{page}</h4>
+              <div className="sidebar-content">
+              {links.map(({ label, icon, url }) => {
+                return (
+                  
+                    <a href={url} className="sidebar-content-item">
+                      {icon}
+                      <span>{label}</span>
+                    </a>
+                 
+                );
+              })}
+               </div>
+            </>
+          );
+        })}
+      </div>
+    </aside>
+  );
 }
